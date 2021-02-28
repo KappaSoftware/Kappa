d4c711675c4a9b56166fc9b7c28556b3e29507a9
German <gd.martinez@uniandes.edu.co>
2021-02-27 19:58:54 -0500

    Habilitar ruta bot

---
+++ kappa-back/routes/bot.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var router = express.Router();
+const [addService] = require("../controllers/locations");
+
+router.post("/", async function (req, res, next) {
+  console.log(req.body);
+  //const entrega = await addEntrega(req.body, actividadId, estudiante._id);
+  res.send("ok");
+});
+
+module.exports = router;
