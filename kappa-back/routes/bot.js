27c9f459f726974c79ad629f0aff16575756733b
German <gd.martinez@uniandes.edu.co>
2021-03-03 21:16:33 -0500

    Update bot.js

--- kappa-back/routes/bot.js
+++ kappa-back/routes/bot.js
@@ -5,7 +5,7 @@ const [addService] = require("../controllers/locations");
 router.post("/", async function (req, res, next) {
   console.log(req.body);
   //const entrega = await addEntrega(req.body, actividadId, estudiante._id);
-  res.send("ok");
+  res.send(req.body);
 });
 
 module.exports = router;
