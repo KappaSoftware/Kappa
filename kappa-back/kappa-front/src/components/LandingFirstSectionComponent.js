78431205b3ae7e702ca57b329a8f14b01805fc0e
German <gd.martinez@uniandes.edu.co>
2021-03-08 00:35:31 -0500

    Mejoras front

--- kappa-back/kappa-front/src/components/LandingFirstSectionComponent.js
+++ kappa-back/kappa-front/src/components/LandingFirstSectionComponent.js
@@ -3,13 +3,39 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import Carousel from "react-bootstrap/Carousel";
 
 export default function LandingFirstSection() {
   return (
     <section className="landing-firstsection" id="ldFirstSec">
       <Container fluid>
         <Row className="landing-firstsection-row row-align-itemvertical">
-          <Col className="landing-firstsection-col">
+          <Col className="landing-firstsection-col pr-0 pl-0">
+            <div className="landing-firstsection-carousel">
+              <Carousel indicators={false}>
+                <Carousel.Item interval={3000}>
+                  <img
+                    className="d-block w-100 landing-firstsection-imageCarousel"
+                    src="/assets/images/LandingFirstSection-Handsmap.jpg"
+                    alt="First slide"
+                  />
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                  <img
+                    className="d-block w-100 landing-firstsection-imageCarousel"
+                    src="/assets/images/LandingFirstSection-GPSHand.jpg"
+                    alt="Second slide"
+                  />
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                  <img
+                    className="d-block w-100 landing-firstsection-imageCarousel"
+                    src="/assets/images/LandingFirstSection-HandPic.jpg"
+                    alt="Third slide"
+                  />
+                </Carousel.Item>
+              </Carousel>
+            </div>
             <div className="landing-firstsection-divText">
               <h1 className="text-color-white landing-firstsection-titleText">
                 Find your locations by categories
