6af58d9ac7fdb4c66a0137f7069875c5e2534a22
German <gd.martinez@uniandes.edu.co>
2021-04-06 23:47:31 -0500

    Landing sections

--- kappa-back/kappa-front/src/components/LandingComponent.js
+++ kappa-back/kappa-front/src/components/LandingComponent.js
@@ -3,7 +3,9 @@ import NavbarLanding from "./NavbarLandingComponent";
 import LandingFirstSection from "./LandingFirstSectionComponent";
 import LandingSecondSection from "./LandingSecondSectionComponent";
 import LandingThirdSection from "./LandingThirdSectionComponent";
-import LandingFourthSection from "./LandingFourthSectionComponent"
+import LandingFourthSection from "./LandingFourthSectionComponent";
+import LandingFifthSection from "./LandingFifthSectionComponent";
+import LandingSixthSection from "./LandingSixthSectionComponent";
 import HomeModalDisclaimer from "./HomeModalDisclaimer";
 
 export default function Landing() {
@@ -14,6 +16,8 @@ export default function Landing() {
       <LandingSecondSection />
       <LandingThirdSection />
       <LandingFourthSection />
+      <LandingFifthSection />
+      <LandingSixthSection />
       <HomeModalDisclaimer />
     </>
   );
---
+++ kappa-back/kappa-front/src/components/LandingSixthSectionComponent.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+
+export default function LandingSixththSection() {
+  return (
+    <section className="landing-sixthsection" id="ldSixthSec">
+      <Container fluid>
+        <Row className="landing-sixthsection-row">
+          <h2 className="landing-sixthsection-text">
+            Kappa gives you the locations of your interest quickly and
+            intuitively
+          </h2>
+        </Row>
+      </Container>
+    </section>
+  );
+}
