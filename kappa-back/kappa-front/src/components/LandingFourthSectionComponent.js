b220e1f0b85454fbcb41f484e10eafb63ee1d4f1
German <gd.martinez@uniandes.edu.co>
2021-04-16 20:50:36 -0500

    Correcciones back y landing

--- kappa-back/kappa-front/src/components/LandingFourthSectionComponent.js
+++ kappa-back/kappa-front/src/components/LandingFourthSectionComponent.js
@@ -2,8 +2,11 @@ import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import { useIntl } from "react-intl";
+import { FaGithub } from "react-icons/fa";
 
 export default function LandingFourthSection() {
+  const intl = useIntl();
   return (
     <section className="landing-fourthsection" id="ldFourthSec">
       <Container fluid>
@@ -13,21 +16,36 @@ export default function LandingFourthSection() {
               <h2>Find specific maps</h2>
               <br />
               <p>
-                Kappa es una iniciativa de impacto social liderada por un equipo
-                multidisciplinar que tiene como objeto brindar al usuario una
-                plataforma de georreferenciación basada en tecnologías libres y
-                abiertas. En la versión actual Kappa permite la
-                georreferenciación de huecos o baches de carretera, centros de
-                vacunación (COVID-19) y eventos (conciertos, actos públicos,
-                manifestaciones, etc.). Mediante nuestro bot en Telegram,
-                nuestros usuarios pueden agregar al mapa cualquiera de estas
-                ubicaciones o consultar las que estén cercanas a su ubicación
-                actual.
+                Kappa es una iniciativa académica de impacto social liderada por
+                un equipo multidisciplinar que tiene como objeto brindar al
+                usuario una plataforma de georreferenciación basada en
+                tecnologías libres y abiertas. En su versión actual, Kappa
+                permite la georreferenciación de huecos o baches de carretera,
+                centros de vacunación (COVID-19) y eventos (conciertos, actos
+                públicos, manifestaciones, etc.). Mediante el bot de Telegram,
+                los usuarios pueden agregar al mapa ubicaciones de lugares o
+                eventos en las categorías mencionadas.
               </p>
               <p>
-                Kappa es Software Libre, cualquier interesado puede contribuir
+                Kappa es Software Libre y cualquier interesado puede contribuir
                 desde nuestro repositorio en GitHub.
               </p>
+              <div
+                className="sidebar-btn-wrapper"
+                style={{
+                  padding: "20px 24px",
+                }}
+              >
+                <a
+                  href="https://github.com/KappaSoftware/Kappa"
+                  target="_blank"
+                  className="landing-fourthsection-btnGitHub"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub />
+                  <span> {intl.formatMessage({ id: "title" })}</span>
+                </a>
+              </div>
             </div>
           </Col>
           <Col xl={6} md={12} className="landing-fourthsection-col">
